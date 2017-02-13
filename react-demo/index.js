865ca3fd7d95c54347b8ecfdf754f1663a98d723
Neil Guo <handsamechina@126.com>
2017-02-13 14:57:01 +0800

    整理打包

--- react-demo/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Escape on 16/7/11.
- */
-var path = require('path'); //用于处理目录的对象
-console.log(path.resolve(__dirname, 'index.js'));
-
--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 /**
- * Created by Escape on 16/7/8.
+ * Created by neil on 16/7/8.
  */
-import _ from 'lodash';
+// import _ from 'lodash';
+
+
 /**
  * @description 格式化url参数 parseUrlQuery
  * @param {url} http://google.com/?id=5&foo=bar
@@ -30,20 +32,6 @@ function serializeObject(obj, separator = '&') {
     `${key}=${obj[key]}`
   ], []).join(separator);
 }
-/**
- * @description 数组排序
- * @param {arr1} 需要排序数组
- * @param {arr2} 需要排序数组
- * @param {fn} 排序条件 func
- * @return []
- */
- function arrSort(arr1, ...rest) {
-   const [arr, fn] = rest;
-   if (_.isFunction(arr)) {
-     return arr1.sort(arr);
-   }
-   return fn ? arr1.sort(fn) : arr1.sort((a, b) => {arr.indexOf(a.id) - arr.indexOf(b.id)});
- }
  /**
   * @description 数组快速排序
   * @param {arr1} 需要排序数组
@@ -70,7 +58,6 @@ function serializeObject(obj, separator = '&') {
  */
 function es6Unique(arr) {
   const items = new Set(arr);
-  //return [...items]; return [...new Set(arr)];
   return Array.from(items);
 }
 /**
@@ -81,7 +68,7 @@ function es6Unique(arr) {
  function unique(arr) {
    const newArr = [];
    arr.forEach(v => {
-     if (newArr.indexOf(v) == -1) newArr.push(v]);
+     if (newArr.indexOf(v) == -1) newArr.push(v);
    });
    return newArr;
  }
@@ -90,7 +77,7 @@ function es6Unique(arr) {
   * @param {on, emit, off, once}
   * @return
   */
-  const Event = ((function() {
+  const myEvent = ((function() {
     let handlers = {};
     function on(evt, func) {
       handlers[evt] = handlers[evt] || [];
@@ -118,56 +105,13 @@ function es6Unique(arr) {
         }
       }
     }
-    function init() {
-      handlers = {};
-    }
     return {
       on,
       once,
       off,
       emit,
-      init,
     };
   })());
-// const unique_Es6 = arr => arr.filter((v, i) => arr.indexOf(v) === i);
-/**
- * @description 图表数据排序
- * @param data   arr
- * @param rowOrCol bool true: 根据首行排序； false: 根据首列排序
- * @return []
- */
-function dataSort(data, rowOrCol) {
-  //当前仅对数据首行/列是数字时进行排序, 否则返回原数组
-  const xData = data[0].slice(1);
-  const yData = data.map(v => v[0]).slice(1);
-  const xAxisData = rowOrCol ? xData : yData;
-  const isNumeber = xAxisData.every((v) => !isNaN(v));
-  if (isNumeber) {
-    //序列数据取行时排序
-    const d = data.map(v => v.slice(1));
-    const xD = d.map(v => {
-      const newArr = []; //转换成对象
-      v.forEach((k, i) => {
-        newArr.push({[d[0][i]]: k});
-      });
-      return newArr;
-    })
-    .map(v => v.sort((a, b) => Object.keys(a) - Object.keys(b))) //根据第一行排序
-    .map((v, i) => {
-      //转回数组,并填充数据第一例
-      const arr = [data.map(k => k[0])[i]];
-      v.forEach((k) => {
-        arr.push(k[Object.keys(k)]);
-      });
-      return arr;
-    });
-    return rowOrCol ? xD : [ // true: 根据数据第一行排序; false: 根据数据第一列排序
-      data[0],
-      ...data.slice(1).sort((a, b) => Number(a[0]) - Number(b[0]))
-    ];
-  }
-  return data;
-}
 /**
  * @description this 软绑定
  * @param obj
@@ -192,10 +136,8 @@ function dataSort(data, rowOrCol) {
 const utils = {
   parseUrlQuery: parseUrlQuery,
   serializeObject: serializeObject,
-  arrSort: arrSort,
   quicksort: quicksort,
-  Event: Event,
-  dataSort: dataSort,
+  myEvent: myEvent,
   softBind: softBind
 }
 export default utils;
